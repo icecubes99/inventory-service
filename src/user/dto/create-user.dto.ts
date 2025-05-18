f07e0e98a94e168679ba0b732de10f887d8b7214
Emmanuel Aguado <aguadojr.emmanueldiaz@gmail.com>
2025-05-18 20:31:17 +0800

    feat: fix eslint errors and made dtos

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,31 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { Role } from '@prisma/client';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsEnum(Role)
+  @IsNotEmpty()
+  role: Role;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { password, ...rest } = createUserDto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await this.prisma.user.create({
+      data: {
+        passwordHash: hashedPassword,
+        ...rest,
+      },
+    });
+
+    if (!user) {
+      throw new Error('User not created');
+    }
+
+    return user;
+  }
+}
