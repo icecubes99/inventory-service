d97497b72d042cf43a439020cacd8a6a5cfbe100
Emmanuel Aguado <aguadojr.emmanueldiaz@gmail.com>
2025-05-25 16:57:15 +0800

    feat: remove temporarily the throttling

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,7 +8,7 @@ import { UserModule } from '../user/user.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TokenBlacklistService } from './token-blacklist.service';
 import * as cookieParser from 'cookie-parser';
-import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+// import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 
 @Module({
@@ -23,21 +23,22 @@ import { APP_GUARD } from '@nestjs/core';
       }),
       inject: [ConfigService],
     }),
-    ThrottlerModule.forRoot([
-      {
-        ttl: 60000,
-        limit: 10,
-      },
-    ]),
+    // TODO: Uncomment this when we have a need to throttle requests
+    // ThrottlerModule.forRoot([
+    //   {
+    //     ttl: 60000,
+    //     limit: 10,
+    //   },
+    // ]),
   ],
   providers: [
     AuthService,
     JwtStrategy,
     TokenBlacklistService,
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard,
-    },
+    // {
+    //   provide: APP_GUARD,
+    //   // useClass: ThrottlerGuard,
+    // },
   ],
   controllers: [AuthController],
   exports: [AuthService],
