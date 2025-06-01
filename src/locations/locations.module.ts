625c91ea0cf9c93b7277958e131b14293b85584d
Emmanuel Aguado <aguadojr.emmanueldiaz@gmail.com>
2025-06-01 16:55:47 +0800

    feat: add userService to the providers of location module

--- src/locations/locations.module.ts
+++ src/locations/locations.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { LocationsController } from './locations.controller';
 import { LocationsService } from './locations.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { PermissionsService } from '../auth/permissions.service';
+import { UserService } from '../user/user.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [LocationsController],
-  providers: [LocationsService],
+  providers: [LocationsService, PermissionsService, UserService],
 })
 export class LocationsModule {}
